f7ca3610f8d7e25a972aa88fc0a452ab25f1017a
John Nelson <jrnelson75032@gmail.com>
2023-09-26 12:41:43 -0500

    Add routes

---
+++ routes/api.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router
+
+router.get('/todos', (res, req, next) => {
+    // get placeholder
+})
+
+router.post('/todos', (res, req, next) => {
+    // post placeholder
+})
+
+router.delete('/todos/:id', (res, req, next) => {
+    // delete placeholder
+})
+
+module.exports = router
