3d08ef9191a95b03d98a3a7deafb938cb4ae3d2b
John Nelson <jrnelson75032@gmail.com>
2023-09-26 14:03:19 -0500

    Connect to DB

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
-const express = require('express')
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const routes = require('./routes/api');
+require('dotenv').config();
 
-const app = express()
+const app = express();
 
-const port= process.env.PORT || 5000
+const port = process.env.PORT || 5000;
+
+// Connect to the database
+mongoose
+  .connect(process.env.DB, { useNewUrlParser: true })
+  .then(() => console.log(`Database connected successfully`))
+  .catch((err) => console.log(err));
+
+// Since mongoose's Promise is deprecated, we override it with Node's Promise
+mongoose.Promise = global.Promise;
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-  });
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
-  app.use((req, res, next) => {
-    res.send('Welcome to Express')
-  })
+app.use(bodyParser.json());
 
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-  })
+app.use('/api', routes);
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  next();
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
